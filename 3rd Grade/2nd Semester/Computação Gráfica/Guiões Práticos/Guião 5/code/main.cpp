50f6b3336e6687a77c9727128c2f460e08488eec
João Moreira <62074012+augusto021@users.noreply.github.com>
2023-05-26 17:42:11 +0100

    Add files via upload

---
+++ 3rd Grade/2nd Semester/Computação Gráfica/Guiões Práticos/Guião 5/code/main.cpp
@@ -0,0 +1,220 @@
+#include <stdio.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+float alfa = 0.0f, beta = 0.5f, radius = 100.0f;
+float camX, camY, camZ;
+
+int r = 50;
+int ri = 35;
+int rc = 15;
+
+void spherical2Cartesian() {
+
+	camX = radius * cos(beta) * sin(alfa);
+	camY = radius * sin(beta);
+	camZ = radius * cos(beta) * cos(alfa);
+}
+
+
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window with zero width).
+	if(h == 0)
+		h = 1;
+
+	// compute window's aspect ratio 
+	float ratio = w * 1.0 / h;
+
+	// Set the projection matrix as current
+	glMatrixMode(GL_PROJECTION);
+	// Load Identity Matrix
+	glLoadIdentity();
+	
+	// Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+
+	// Set perspective
+	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+
+	// return to the model view matrix mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+void renderScene(void) {
+
+	// clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// set the camera
+	glLoadIdentity();
+	gluLookAt(camX, camY, camZ,
+		0.0, 0.0, 0.0,
+		0.0f, 1.0f, 0.0f);
+	
+	// put code to draw scene in here
+	
+	// ------------ Plane ------------ //
+	glColor3f(0.2f, 0.8f, 0.2f);
+	glBegin(GL_TRIANGLES);
+		glVertex3f(100.0f, 0, -100.0f);
+		glVertex3f(-100.0f, 0, -100.0f);
+		glVertex3f(-100.0f, 0, 100.0f);
+
+		glVertex3f(100.0f, 0, -100.0f);
+		glVertex3f(-100.0f, 0, 100.0f);
+		glVertex3f(100.0f, 0, 100.0f);
+	glEnd();
+	// ------------------------------ //
+
+	// ---------- Inner Teapots ---------- //
+
+	glColor3ub(0, 0, 255);
+	for(int i = 0;i < 8;i++){
+		glPushMatrix();
+
+		glRotatef(45.0 * i,0,1,0);
+		glTranslatef(rc,1,0);
+		glutSolidTeapot(2);
+
+		glPopMatrix();
+	}
+	// ------------------------------------ //
+
+	// ---------- Outer Teapots ---------- //
+
+	glColor3ub(255, 0, 0);
+	for(int i = 0;i < 16;i++){
+		glPushMatrix();
+
+		glRotatef(22.5 * i,0,1,0);
+		glTranslatef(ri,1,0);
+		glutSolidTeapot(2);
+
+		glPopMatrix();
+	}
+	// ----------------------------------- //
+
+	// ---------- Torus ---------- //
+	glColor3ub(255,0,255);
+	glutSolidTorus(1,3,10,10);
+	// --------------------------- //
+	
+	// ---------- Trees ---------- //
+
+	for(int i = 0;i < 300;i++){
+		glPushMatrix();
+
+		float posx = 0, posz = 0;
+		while(pow(posx,2) + pow(posz,2) < pow(r,2)){
+			posx = (rand() % (r * 4)) - (r * 2);
+			posz = (rand() % (r * 4)) - (r * 2);
+		}
+		glTranslatef(posx,0,posz);
+		glRotatef(-90,1,0,0);
+		glColor3ub(102,62,36);
+		glutSolidCone(1,3,10,10);
+		glTranslatef(0,0,2.5);
+		glColor3ub(0,255,0);
+		glutSolidCone(3,6,10,10);
+		glPopMatrix();
+	}
+
+	// --------------------------- //
+	
+	glutSwapBuffers();
+}
+
+
+void processKeys(unsigned char c, int xx, int yy) {
+
+// put code to process regular keys in here
+
+}
+
+
+void processSpecialKeys(int key, int xx, int yy) {
+
+	switch (key) {
+
+	case GLUT_KEY_RIGHT:
+		alfa -= 0.1; break;
+
+	case GLUT_KEY_LEFT:
+		alfa += 0.1; break;
+
+	case GLUT_KEY_UP:
+		beta += 0.1f;
+		if (beta > 1.5f)
+			beta = 1.5f;
+		break;
+
+	case GLUT_KEY_DOWN:
+		beta -= 0.1f;
+		if (beta < -1.5f)
+			beta = -1.5f;
+		break;
+
+	case GLUT_KEY_PAGE_DOWN: radius -= 1.0f;
+		if (radius < 1.0f)
+			radius = 1.0f;
+		break;
+
+	case GLUT_KEY_PAGE_UP: radius += 1.0f; break;
+	}
+	spherical2Cartesian();
+	glutPostRedisplay();
+
+}
+
+
+void printInfo() {
+
+	printf("Vendor: %s\n", glGetString(GL_VENDOR));
+	printf("Renderer: %s\n", glGetString(GL_RENDERER));
+	printf("Version: %s\n", glGetString(GL_VERSION));
+
+	printf("\nUse Arrows to move the camera up/down and left/right\n");
+	printf("Home and End control the distance from the camera to the origin");
+}
+
+
+int main(int argc, char **argv) {
+
+// init GLUT and the window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(800,800);
+	glutCreateWindow("CG@DI-UM");
+		
+// Required callback registry 
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	
+// Callback registration for keyboard processing
+	glutKeyboardFunc(processKeys);
+	glutSpecialFunc(processSpecialKeys);
+
+//  OpenGL settings
+	glEnable(GL_DEPTH_TEST);
+	//glEnable(GL_CULL_FACE);
+
+	spherical2Cartesian();
+
+	printInfo();
+
+// enter GLUT's main cycle
+	glutMainLoop();
+	
+	return 1;
+}
